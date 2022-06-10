31c0f1a9445b5c33894fb71f2ed3e2168bf1c42b
LuminaEXE <86614978+LuminaEXE@users.noreply.github.com>
2022-06-10 16:07:37 -0500

    Add files via upload

--- shop/script.js
+++ shop/script.js
@@ -1,7 +1,35 @@
 
+const staffRoles = document.getElementById("staff-roles")
+const lgTitle = document.getElementById("lgTitle")
+const usernameInput = document.getElementById("usernameInput") // <-- Lg = Login
+const lgInput1 = document.getElementById("lgInput1")
+const login = document.getElementById("login")
+
+function hideLogin() {
+    login.style.display = "none"
+}
+
+function showMainPage() {
+    staffRoles.style.display = "block"
+    
+}
+
+
+if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+    
+    staffRoles.style.display = "none"
+    
+    document.getElementById("login").style.display = "block"
+    
+}
+
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const cometicSpan = document.querySelector('cape-name');
+
+
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -17,150 +45,89 @@ function closeMenu() {
 }
 
 
+let skinViewer = new skinview3d.SkinViewer({
+    canvas: document.getElementById("skin_container"),
+    width: 350,
+    height: 400
+    
+});
+
+
+function fetchCosmetics() {
+    var uuid = document.getElementById("uuidInput").value;
+    skinViewer.loadSkin("https://visage.surgeplay.com/skin/"+uuid);
+    fetch("http://api.glacierclient.net/user/assets/equipedCape/txt/" + uuid)
+        .then(response => response.json())
+        .then(data =>{ 
+            cosmeticSpan.textContent = response;
+        })
+
+}
+skinViewer.loadCape("../resources/capes/GlacierBlue.png");
+skinViewer.fov = 65;
+
+var uuid = "c5f74dff38cc4953994e615752bf22cd";
+
+
+
+skinViewer.camera.rotation.z = -3.08
+skinViewer.camera.rotation.y = -0.5
+skinViewer.camera.rotation.x = -3.025
+skinViewer.camera.position.z = -30
+skinViewer.camera.position.y = 2.75
+skinViewer.camera.position.x = -15
+
+
+function cosmeticsConstructor(_CosmName, _CosmType, _CosmCost) {
+
+    openModal()
+    
+    const cosmetic = document.getElementById("profile-container");
+    const cosmeticName = document.getElementById("username");
+    const cosmeticType = document.getElementById("type");
+    const cosmeticCost = document.getElementById("cost");
+
+    cosmeticName.textContent = _CosmName;
+    cosmeticType.textContent = _CosmType;
+    cosmeticCost.textContent = _CosmCost;
+    console.log("clicked")
+}
 
 function closeModal() {
     document.getElementById("profile-modal").style.display="none";
 }
 
-
-function myYugo(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "Yugo"
-    document.getElementById("role").innerHTML = "Owner"
-    document.getElementById("discord").innerHTML = "Discord: Yugo#9897"
-    document.getElementById("email").innerHTML = "Email: yugo@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"I found this client, I win"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/840004980517896242";
-
-}
-function mySpy(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "SpyMiner"
-    document.getElementById("role").innerHTML = "Owner/Developer"
-    document.getElementById("discord").innerHTML = "Discord: SpyMiner#0001"
-    document.getElementById("email").innerHTML = "Email: spyminer@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"I may be drunk, Miss, but in the morning I will be sober and you will still be ugly."'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/418323201811152897";
-}
-function mySun(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "iBrokenSun"
-    document.getElementById("role").innerHTML = "Senior Manager"
-    document.getElementById("discord").innerHTML = "Discord: iBrokenSun#7381"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"Person with a weird pfp"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/652858814009442335";
+function openModal() {
+    document.getElementById("profile-modal").style.display="inline-block";
 }
 
-function myKrezio(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "Krezio"
-    document.getElementById("role").innerHTML = "Manager"
-    document.getElementById("discord").innerHTML = "Discord: Krezio#3320"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"A Frog"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/775986083871064075";
-    console.log('test')
+
+function fetchAndLogin() {
+    username = usernameInput.value;
+	fetch("https://api.glacierclient.net/user/playerUUID/"+username)
+    .then(
+        response => {
+            return response.json();
+        }
+    )
+    .then(
+        data => {
+            const uuid= data.UUID;
+            console.log(
+                uuid,
+                username
+            );
+        }
+    );
+    if(!username){
+        alert("username not not vaild or or not found")
+        location.reload();
+    }
+    else {
+        setTimeout(function(){
+            showMainPage();
+            hideLogin()
+        }, 1000);
+        lgTitle.innerText = "logged in as " + username
+    }
 }
-
-function myFelix(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "Felix"
-    document.getElementById("role").innerHTML = "Developer"
-    document.getElementById("discord").innerHTML = "Discord: Felixmax_#6450"
-    document.getElementById("email").innerHTML = "Email: felix@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"The guy who makes the wierd starty thing"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/460470667696013312";
-}
-function myDuke(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "Duke"
-    document.getElementById("role").innerHTML = "Developer"
-    document.getElementById("discord").innerHTML = "Discord: Duke#6377"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"Someone who made what you are looking at right now 👀"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/835806614141141001";
-}
-function myMoon(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "MoonStyle"
-    document.getElementById("role").innerHTML = "Designer"
-    document.getElementById("discord").innerHTML = "Discord: BrokenMoon#2002    "
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"Designs stuff"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/466862322581897226";
-
-}
-function myLumina(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "Lumina"
-    document.getElementById("role").innerHTML = "Developer"
-    document.getElementById("discord").innerHTML = "Discord: Lumina#3384"
-    document.getElementById("email").innerHTML = "Email: Lumina@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"Dont you ever say anything about my babe JS"'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/741623363612704789";
-
-}
-function myAustin(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "AustinFRG"
-    document.getElementById("role").innerHTML = "Support"
-    document.getElementById("discord").innerHTML = "Discord: AustinFRG#0001"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"Talks I guess."'
-    document.getElementById("picture").src="https://api.glacierclient.net/assets/discord/pfp/298226178576220170";
-}
-function myCreeper(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "thedcreeper"
-    document.getElementById("role").innerHTML = "Support"
-    document.getElementById("discord").innerHTML = "Discord: thedrcreeper#9892"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"N/A"'
-    document.getElementById("picture").src="https://cdn.discordapp.com/avatars/794279247396864000/09cdde75cfa885559d275a20d0c28b8a.png?size=2048";
-}
-
-function myGrilla(){
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = "perionaa"
-    document.getElementById("role").innerHTML = "Support"
-    document.getElementById("discord").innerHTML = "Discord: Grilla#9143"
-    document.getElementById("email").innerHTML = "Email: contact@glacierclient.net"
-    document.getElementById("quote").innerHTML = '"N/A"'
-    document.getElementById("picture").src="https://cdn.discordapp.com/avatars/858336884312768512/1107fc4a0f3bde5efa7359867c08662a.png?size=2048";
-}
-/* test | will remain there cuz im lazy wont harm*/
-
-const Lumina = document.getElementById("Lumina");
-Lumina.addEventListener("click", myLumina);
-
-
-/*
-function userConstructer(username, role, discord, email, quote, picture) {
-    
-    if(username || role || discord || email || quote || picture === null || NaN){
-        throw new Error("Missing argument");
-    }   
-
-
-    document.getElementById("profile-modal").style.display="block";
-    document.getElementById("username").innerHTML = username
-    document.getElementById("role").innerHTML = "Role: " + role
-    document.getElementById("discord").innerHTML = "Discord: " + discord
-    document.getElementById("email").innerHTML = "Email: " +email
-    document.getElementById("quote").innerHTML = '"${quote}"'
-    document.getElementById("picture").src=picture;
-
-
-}
-*/
-
-//TODO: implementation of userConstructer
-
-
-
-
-
-
-
-
