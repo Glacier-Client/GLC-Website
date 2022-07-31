446702f4f8fc37eb48f233d75cbc57558e304a22
SpyMIner <vince051105@icloud.com>
2022-07-31 19:57:14 +0200

    Update script.js

--- home/script.js
+++ home/script.js
@@ -7,8 +7,8 @@ const getOnlineCounter = document.getElementById("OnlineCounter");
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-let players = fetch("https://api.glacierclient.net/stats/currentlyOnline", {method : "GET", headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials": true,},});
-players.then(response => {return response.json();}).then(data => {const number = data.Online; getOnlineCounter.textContent = number;});
+let players = fetch("https://api.glacierclient.net/stats/downloads", {method : "GET", headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin" : "*", "Access-Control-Allow-Credentials": true,},});
+players.then(response => {return response.json();}).then(data => {const number = data.Downloads; getOnlineCounter.textContent = number;});
     
 function mobileMenu() {
     hamburger.classList.toggle("active");
